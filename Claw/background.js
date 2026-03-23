abce4315770017063f14a9d117bd93540ef6b0d7
onerd.dev <emanuelfelipe.120309@gmail.com>
2026-03-23 08:36:19 -0300

    Delete Claw directory

--- Claw/background.js
+++
@@ -1,5 +0,0 @@
-/* CLAW — background.js | Desenvolvido por Emanuel Felipe */
-chrome.runtime.onMessage.addListener((msg, sender, reply) => {
-  if (msg.type === 'PING') reply({ ok: true });
-  return false;
-});
